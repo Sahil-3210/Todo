b3629ae60755d2bbcf1292a735f59554ddbbb520
Sahil Randive <randivesahil7@gmail.com>
2024-04-22 13:56:43 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <TodoProvider value={{todos, addTodo, deleteTodo, updatedTodo, toggleComplete}}>
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#172554] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
